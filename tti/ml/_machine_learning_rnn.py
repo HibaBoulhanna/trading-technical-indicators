61f5ac68b632a7b3b6f404002d96a649bf3c6eac
vsaveris <vsaveris@gmail.com>
2021-01-03 12:34:30 +0100

    feat: Implement a Recurrent Neural Network.
    New module introduced for the implementation of a Recurrent Neural
    Network for priced direction prediction. Related to the issue #30.

---
+++ tti/ml/_machine_learning_rnn.py
@@ -0,0 +1,16 @@
+"""
+Trading-Technical-Indicators (tti) python library
+
+File name: _machine_learning_rnn.py
+    Implements a Recurrent Neural Network for the Machine Learning features of
+    the tti library.
+"""
+
+
+class MachineLearningRNN:
+    """
+    Machine Learning RNN class implementation.
+    """
+
+    def __init__(self):
+        raise NotImplementedError
