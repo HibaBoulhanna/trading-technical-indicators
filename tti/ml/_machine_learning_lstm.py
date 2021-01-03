636936df1bc1e86f71a73f187f64d45ea4c677fb
vsaveris <vsaveris@gmail.com>
2021-01-03 12:43:09 +0100

    feat: Implement a Long Short Term Memory Neural Network.
    New module introduced for the implementation of a Long Short Term Memory
    Neural Network for priced direction prediction. Related to the issue

---
+++ tti/ml/_machine_learning_lstm.py
@@ -0,0 +1,16 @@
+"""
+Trading-Technical-Indicators (tti) python library
+
+File name: _machine_learning_rnn.py
+    Implements a Long Short Term Memory Neural Network for the Machine Learning
+    features of the tti library.
+"""
+
+
+class MachineLearningLSTM:
+    """
+    Machine Learning LSTM class implementation.
+    """
+
+    def __init__(self):
+        raise NotImplementedError
