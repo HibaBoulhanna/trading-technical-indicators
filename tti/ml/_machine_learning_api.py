e3589b52ede737915677e56732e8453b88fe20f4
vsaveris <vsaveris@gmail.com>
2021-01-09 21:47:52 +0100

    feat: Machine Learning API updates.
    Machine Learning API updated with scaler details.

--- tti/ml/_machine_learning_api.py
+++ tti/ml/_machine_learning_api.py
@@ -8,7 +8,7 @@ File name: _machine_learning_api.py
 import pickle
 
 from abc import ABC, abstractmethod
-from ..utils.constants import DEFAULT_TI_FEATURES, ML_CLASSES
+from ..utils.constants import ALL_TI_FEATURES, ML_CLASSES
 
 
 class MachineLearningAPI(ABC):
@@ -22,19 +22,26 @@ class MachineLearningAPI(ABC):
     Attributes:
         _model (object): The trained model object.
 
+        _scaler (sklearn.preprocessing.StandardScaler): Scaler used with the
+            training data.
+
         _model_details (dict): Dictionary with model details. It contains the
             ``model_type`` see model_type argument, ``training_score`` the
             score of the trained model on the training data, ``test_score`` the
             score of the trained model on the test data,
             ``number_of_training_instances`` the number of instances used to
             train the model, ``classes`` a definition (dict) of the classes,
-            ``dump_file`` the file where the model is staved.
+            ``scaler_used`` whether a scaler used with the training data, and
+            ``dump_file`` the file where the model is saved.
     """
     
     def __init__(self, model_type):
 
         # Trained model, when None there is not any trained model yet
         self._model = None
+
+        # Data scaler, when None there is not any scaler used
+        self._scaler = None
 
         # Training score, test score, number of training instances are set by
         # the child class
@@ -43,6 +50,7 @@ class MachineLearningAPI(ABC):
                                'test_score': None,
                                'number_of_training_instances': None,
                                'classes': ML_CLASSES,
+                               'scaler_used': False,
                                'dump_file': None}
 
     def mlSaveModel(self, file_name):
@@ -56,8 +64,12 @@ class MachineLearningAPI(ABC):
 
         self._model_details['dump_file'] = file_name
 
+        self._model_details['scaler_used'] = False if self._scaler is None \
+            else True
+
         with open(file_name, 'wb') as out_file:
-            pickle.dump((self._model, self._model_details), out_file)
+            pickle.dump((self._scaler, self._model, self._model_details),
+                        out_file)
 
     def mlLoadModel(self, file_name):
         """
@@ -69,7 +81,8 @@ class MachineLearningAPI(ABC):
         """
 
         with open(file_name, 'rb') as in_file:
-            self._model, self._model_details = pickle.load(in_file)
+            self._scaler, self._model, self._model_details = \
+                pickle.load(in_file)
 
     def mlModelDetails(self):
         """
@@ -80,37 +93,62 @@ class MachineLearningAPI(ABC):
             details.
         """
 
+        self._model_details['scaler_used'] = False if self._scaler is None \
+            else True
+
         return self._model_details
 
     @abstractmethod
-    def mlTrainModel(self, input_data, ti_features=DEFAULT_TI_FEATURES,
-                     price_diff_periods=1, pool_size=None, verbose=False):
+    def mlTrainModel(self, input_data, ti_features=ALL_TI_FEATURES,
+                 include_close_feature=False, include_volume_feature=False,
+                 price_diff_periods=1, pool_size=None, verbose=False):
         """
         Trains a machine learning model for prices direction predictions.
 
         Args:
-        input_data (pandas.DataFrame): The input data. Required input columns
-            are dependent from the technical indicators which will be used as
-            features (see ti_features argument). The largest column set is
-            ``high``, ``low``, ``open``, ``close`` and ``volume``. The index is
-            of type ``pandas.DatetimeIndex``.
+            input_data (pandas.DataFrame): The input data. Required input
+                columns are dependent from the technical indicators which will
+                be used as features (see ti_features argument). The largest
+                column set is ``high``, ``low``, ``open``, ``close`` and
+                ``volume``. The index is of type ``pandas.DatetimeIndex``. The
+                ``close`` and ``volume`` columns are required in case they are
+                included also as features (see ``include_close_feature`` and
+                ``include_volume_feature`` arguments).
 
-        ti_features (list, default=DEFAULT_TI_FEATURES): List of indicators to
-            be used as features for the ML data. The format of the list is
-            ``[{'ti': 'indicator_class_name', 'kwargs': {...}, ...]``. Use
-            ``DEFAULT_TI_FEATURES`` as usage example. The default value is to
-            use all the 62 indicators.
+            ti_features (list or None, default=ALL_TI_FEATURES): List of
+                indicators to be used as features for the ML data. The format
+                of the list is ``[{'ti': 'indicator_class_name', 'kwargs':
+                {...}, ...]``. See ``ALL_TI_FEATURES`` as usage example. The
+                default value is to use all the 62 indicators. If None, or
+                empty list is given, then none ti signal is included as feature
+                in the created ML data.
 
-        price_diff_periods (int, default=1): The price periods distance for
-            which the price direction is evaluated. The default value is for
-            ``1`` period (i.e. 1 day).
+            include_close_feature (bool, default=False): Indicates whether the
+                ``close`` value from the input data should be included as a
+                feature in the produced ML data.
 
-        pool_size (int, default=None): Pool size of the processes used when
-            concurrency is applied. When None, then the created processes are
-            equal to the number of the available cpu cores.
+            include_volume_feature (bool, default=False): Indicates whether the
+                ``volume`` value from the input data should be included as a
+                feature in the produced ML data.
 
-        verbose (bool, default=False): If set to True, processing information
-            is sent to the console.
+            price_diff_periods (int, default=1): The number of periods ahead
+                for which the price direction is evaluated. The default value
+                is for ``1`` period (i.e. 1 day).
+
+            pool_size (int, default=None): Pool size parallel computing when
+                concurrency applies.  When None, then the created processes are
+                equal to the number of the available cpu cores.
+
+            verbose (bool, default=False): If set to True, processing
+                information is sent to the console.
+
+        Raises:
+            WrongTypeForInputParameter: Input argument has wrong type
+            WrongValueForInputParameter: Unsupported value for input argument
+            NotEnoughInputData: Not enough data for calculating the indicator
+            TypeError: Type error occurred when validating the ``input_data``
+            ValueError: Value error occurred when validating the ``input_data``
+            NoFeaturesSelectedForMLData: No features selected for ML data
         """
 
         raise NotImplementedError
